d31d48ce34700009d095251026f2994bfbe5a744
Muhammad Kashif <muhammadkashif8078@gmail.com>
2024-05-18 19:09:48 +0500

    complete

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -5,10 +5,20 @@ const Navigation = () => {
         <img src="src\assets\brand_logo.png" alt="logo" />
       </div>
       <ul>
-        <li href="#">Menu</li>
-        <li href="#">Location</li>
-        <li href="#">About</li>
-        <li href="#">Contact</li>
+        <a href="#">
+          <li>Menu</li>
+        </a>{" "}
+        <a href="#">
+          {" "}
+          <li>Location</li>
+        </a>
+        <a href="#">
+          <li>About</li>
+        </a>
+        <a href="#">
+          {" "}
+          <li>Contact</li>
+        </a>
       </ul>
 
       <button>login</button>
