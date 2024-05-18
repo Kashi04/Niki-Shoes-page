a4b365491ac673672561163658d7d25f8f171c1b
Muhammad Kashif <muhammadkashif8078@gmail.com>
2024-05-18 18:06:58 +0500

    complete project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import MainPage from "./components/MainPage";
+import Navigation from "./components/Navigation";
+import "./App.css";
+function App() {
+  return (
+    <div>
+      <Navigation />
+      <MainPage />
+    </div>
+  );
+}
+
+export default App;
